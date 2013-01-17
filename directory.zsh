c458f052a62377b1b3c978d206e3c407771e1564
yushengjun <apieceofredcloth@gmail.com>
2013-01-17 21:19:45 +0800

    crlf conf

--- directory.zsh
+++ directory.zsh
@@ -1,8 +1,2 @@
-for x (fang admin); do
-    hash -d $x="$HOME/repos/${x}site/"
-    hash -d ${x}deploy="$HOME/repos/${x}site/deploy/"
-    hash -d ${x}django="$HOME/repos/${x}site/module/ssldjango/"
-    hash -d ${x}webroot="$HOME/repos/${x}site/webroot/"
-done
 hash -d log="/var/log/tiger/"
 hash -d tiger="/opt/tiger/"
